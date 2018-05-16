89fa24f2a1c16621c209e2d436d1c6b647baab9e
bradleymiller17 <36234270+bradleymiller17@users.noreply.github.com>
2018-05-16 14:01:03 -0400

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using U4_SpaceInvaders;
+
+namespace U4_SpaceInvaders
+{
+    class Bullet
+    {
+        Point bulletPos = new Point();
+        private Point point;
+        public Point Point { get => point; }
+        Canvas canvas;
+        MainWindow window;
+        Rectangle bulletRectangle;
+
+        public Bullet(Canvas c, MainWindow w)
+        {
+            //Generate Bullet
+            canvas = c;
+            window = w;
+            point = new Point(346, 558);
+            bulletPos = point;
+            bulletRectangle = new Rectangle();
+            bulletRectangle.Fill = Brushes.Red;
+            bulletRectangle.Height = 32;
+            bulletRectangle.Width = 8;
+            canvas.Children.Add(bulletRectangle);
+            Canvas.SetTop(bulletRectangle, point.Y);
+            Canvas.SetLeft(bulletRectangle, point.X);
+        }
+
+        public void Tick()
+        {
+            bulletPos.Y = bulletPos.Y + 3;
+
+        }
+    }
+}
