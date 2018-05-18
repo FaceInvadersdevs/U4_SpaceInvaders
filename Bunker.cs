7694d079cccae4cdcab73aff50a5616f4b012909
bradleymiller17 <36234270+bradleymiller17@users.noreply.github.com>
2018-05-18 14:57:58 -0400

    Add files via upload

--- Bunker.cs
+++ Bunker.cs
@@ -15,24 +15,40 @@ namespace U4_spaceInvaders
     class Bunker
     {
         //Generate Player Variables
-        Point playerPos = new Point();
+        Point bunkerPos = new Point();
         private Point point;
         public Point Point { get => point; }
         Canvas canvas;
         MainWindow window;
-        Rectangle playerRectangle;
+        Rectangle bunkerRectangle;
 
         //Create Sprites
-        ImageBrush sprite_F_MoveLeft = new ImageBrush(new BitmapImage(new Uri("Faceship_Move_Left.png", UriKind.Relative)));
-        ImageBrush sprite_F_MoveRight = new ImageBrush(new BitmapImage(new Uri("Faceship_Move_Right.png", UriKind.Relative)));
-        ImageBrush sprite_F_FaceShoot = new ImageBrush(new BitmapImage(new Uri("Faceship_Shoot.png", UriKind.Relative)));
-        ImageBrush sprite_F_Spaceship = new ImageBrush(new BitmapImage(new Uri("Faceship.png", UriKind.Relative)));
+        ImageBrush sprite_Bunker1 = new ImageBrush(new BitmapImage(new Uri("Bunker1.png", UriKind.Relative)));
+        ImageBrush sprite_Bunker2 = new ImageBrush(new BitmapImage(new Uri("Bunker2.png", UriKind.Relative)));
+        ImageBrush sprite_Bunker3 = new ImageBrush(new BitmapImage(new Uri("Bunker3.png", UriKind.Relative)));
+        ImageBrush sprite_Bunker4 = new ImageBrush(new BitmapImage(new Uri("Bunker4.png", UriKind.Relative)));
+        ImageBrush sprite_Bunker5 = new ImageBrush(new BitmapImage(new Uri("Bunker5.png", UriKind.Relative)));
 
-        ImageBrush sprite_S_MoveLeft = new ImageBrush(new BitmapImage(new Uri("Spaceship_Move_Left.png", UriKind.Relative)));
-        ImageBrush sprite_S_MoveRight = new ImageBrush(new BitmapImage(new Uri("Spaceship_Move_Right.png", UriKind.Relative)));
-        ImageBrush sprite_S_SpaceShoot = new ImageBrush(new BitmapImage(new Uri("Spaceship_Shoot.png", UriKind.Relative)));
-        ImageBrush sprite_S_Spaceship = new ImageBrush(new BitmapImage(new Uri("Spaceship.png", UriKind.Relative)));
-
-        
+        public Bunker(Canvas c, MainWindow w)
+        {
+            //Generate Bunker
+            canvas = c;
+            window = w;
+            point = new Point(92, 629);
+            bunkerPos = point;
+            bunkerRectangle = new Rectangle();
+            bunkerRectangle.Fill = Brushes.Green;
+            bunkerRectangle.Height = 64;
+            bunkerRectangle.Width = 64;
+            canvas.Children.Add(bunkerRectangle);
+            Canvas.SetTop(bunkerRectangle, point.Y);
+            Canvas.SetLeft(bunkerRectangle, point.X); 
+        }
     }
 }
+
+
+
+
+
+
